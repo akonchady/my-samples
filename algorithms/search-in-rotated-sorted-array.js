ee9e92c2c8fe2ed4e9879aa74cd7fe387c95cd98
Adarsh Konchady <akonchady@gmail.com>
2016-03-12 01:38:08 +0530

    ALGO + html5,css3,sass-less notes

--- algorithms/search-in-rotated-sorted-array.js
+++ algorithms/search-in-rotated-sorted-array.js
@@ -13,22 +13,30 @@ function search(arr, low, high, key) {
     if(arr[mid] == key) {
         return mid;
     }
+
+    /* If arr[l...mid] is sorted */
     if(arr[low] <= arr[mid]) {
-        //left half is sorted
+        /* As this subarray is sorted, we can quickly
+         check if key lies in half or other half */
         if(arr[low] <= key && arr[mid] >= key) {
             return search(arr, low, mid-1, key);
         }
-        else {
-            return search(arr, mid+1, high, key);
-        }
+
+        // if key is not present in left half..search right half.
+        return search(arr, mid+1, high, key);
     }
     else {
-        //right half is sorted
-        if(arr[mid] <= key && arr[high] >= key) {
+        /* If arr[l..mid] is not sorted, then arr[mid... r]
+         must be sorted*/
+        if(arr[mid] <= key && arr[high] >= key) {  // if key is present in right half.
             return search(arr, mid+1, high, key);
         }
-        else {
-            return search(arr, low, mid-1, key);
-        }
+
+        // if key is not present in right half..search in left half.
+        return search(arr, low, mid-1, key);
     }
 }
+
+var arr = [4,5,6,7,8,9,1,2,3];
+
+console.log(search(arr, 0, arr.length - 1, 7));
--- examples/geeks4geeks/6-searchInSortedRotatedArray.js
+++ examples/geeks4geeks/6-searchInSortedRotatedArray.js
@@ -6,6 +6,7 @@
 /*
 Solution 1: O(log n)
  */
+/*
 var arr = [4,5,6,7,8,9,1,2,3];
 // var arr = [1,2,3,4,5];
 
@@ -81,4 +82,47 @@ function pivotedBinarySearch(arr, ele, l, h) {
     }
 }
 
-console.log(pivotedBinarySearch(arr,4, 0, arr.length - 1));
+console.log(pivotedBinarySearch(arr,4, 0, arr.length - 1));*/
+
+
+/*
+ Solution 2: O(log n)
+ */
+// We can search an element in one pass of Binary Search.
+
+function search(arr, low, high, key) {
+    mid = Math.floor((low+high)/2);
+
+    if(low > high) {
+        return -1;
+    }
+
+    if(arr[mid] == key) {
+        return mid;
+    }
+
+    /* If arr[l...mid] is sorted */
+    if(arr[low] <= arr[mid]) {
+        /* As this subarray is sorted, we can quickly
+         check if key lies in half or other half */
+        if(arr[low] <= key && arr[mid] >= key) {
+            return search(arr, low, mid-1, key);
+        }
+
+        // if key is not present in left half..search right half.
+        return search(arr, mid+1, high, key);
+    }
+    else {
+        /* If arr[l..mid] is not sorted, then arr[mid... r]
+         must be sorted*/
+        if(arr[mid] <= key && arr[high] >= key) {  // if key is present in right half.
+            return search(arr, mid+1, high, key);
+        }
+
+        // if key is not present in right half..search in left half.
+        return search(arr, low, mid-1, key);
+    }
+}
+
+var arr = [4,5,6,7,8,9,1,2,3], key = 9;
+console.log('Element ' + key + ' found at index ' + search(arr, 0, arr.length - 1, key));
