96ebc39a45d5ad7ea1f95d794224e64d00018d23
Adarsh Konchady <akonchady@gmail.com>
2018-02-26 22:57:48 +0530

    Fixed include path for babel loader

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, '../src'),
         loader: 'babel-loader',
         exclude: /node_modules/
       }
